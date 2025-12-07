d4ea66a6827013e65f851993c8e29939ec3e3a47
beanz <79058+beanz@users.noreply.github.com>
2025-12-07 13:50:14 +0000

    2024 Q12 Go

--- 2024/12/ec.go
+++ 2024/12/ec.go
@@ -11,7 +11,6 @@ import (
 
 func parts(i1, i2, i3 []byte) (int, int, int) {
 	p1 := parse(i1)
-	Lg(p1)
 	var a1 int
 	{
 		for _, t := range p1.targets {
@@ -20,7 +19,6 @@ func parts(i1, i2, i3 []byte) (int, int, int) {
 	}
 
 	p2 := parse(i2)
-	Lg(p2)
 	var a2 int
 	{
 		for _, t := range p2.targets {
@@ -30,11 +28,17 @@ func parts(i1, i2, i3 []byte) (int, int, int) {
 			a2 += 2 * p2.power(t)
 		}
 	}
-	return a1, a2, 0
+	p3 := parse3(i3)
+	var a3 int
+	for _, t := range p3.targets {
+		a := p3.power3(t)
+		a3 += a
+	}
+	return a1, a2, a3
 }
 
 func main() {
-	p1, p2, p3 := parts(Input(1, ""), Input(2, ""), Input(3, "ex"))
+	p1, p2, p3 := parts(Input(1, ""), Input(2, ""), Input(3, ""))
 	fmt.Printf("Part 1: %v\nPart 2: %v\nPart 3: %v\n", p1, p2, p3)
 }
 
@@ -52,6 +56,18 @@ type parsed struct {
 	segment     []*point
 }
 
+func parse3(in []byte) *parsed {
+	p := &parsed{segment: []*point{{1, 1}, {1, 2}, {1, 3}}}
+	for i := 0; i < len(in); i++ {
+		j, x := ChompUInt[int](in, i)
+		j, y := ChompUInt[int](in, j+1)
+		i = j
+		t := point{x, y}
+		p.targets = append(p.targets, &t)
+	}
+	return p
+}
+
 func parse(in []byte) *parsed {
 	p := &parsed{segment: make([]*point, 3)}
 	w := bytes.IndexByte(in, '\n') + 1
@@ -59,13 +75,14 @@ func parse(in []byte) *parsed {
 	for y := h - 1; y >= 0; y-- {
 		for x := 0; x < w-1; x++ {
 			i := x + (h-y)*w
+			t := point{x, y}
 			switch in[i] {
 			case 'A', 'B', 'C':
-				p.segment[in[i]-'A'] = &point{x, y}
+				p.segment[in[i]-'A'] = &t
 			case 'T':
-				p.targets = append(p.targets, &point{x, y})
+				p.targets = append(p.targets, &t)
 			case 'H':
-				p.hardTargets = append(p.hardTargets, &point{x, y})
+				p.hardTargets = append(p.hardTargets, &t)
 			}
 		}
 	}
@@ -88,6 +105,26 @@ func (p *parsed) power(t *point) int {
 		}
 	}
 	return mn
+}
+
+func (p *parsed) power3(m *point) int {
+	x := m.x / 2
+	y := m.y - x - m.x&0x1
+	for i, s := range p.segment {
+		dx := x - s.x + 1
+		dy := y - s.y + 1
+		if dx < dy {
+			continue
+		}
+
+		if dx <= 2*dy {
+			return dy * (i + 1)
+		}
+		if px3 := dx + dy; px3%3 == 0 {
+			return (px3 / 3) * (i + 1)
+		}
+	}
+	return -9999
 }
 
 func (p *parsed) String() string {
